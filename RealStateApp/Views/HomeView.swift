a48ae7bcff4d90f5bfbd010f45f9229329b8fc4d
Brandom <brandon.rsantillan@gmail.com>
2023-11-30 22:31:38 -0500

    init

---
+++ RealStateApp/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  RealStateApp
+//
+//  Created by Brandon Rojas on 30/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Something")
+    }
+}
+
+#Preview {
+    HomeView()
+}
